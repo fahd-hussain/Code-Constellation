98b77d3a0fc61c7b101370f34966bf759beeb539
Fahad Hussain <fahd__hussain@hotmail.com>
2024-07-18 00:43:04 +0500

    Introduced Colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-4'>
+    <main className='flex min-h-screen flex-col items-center justify-center gap-4 bg-cyan-100'>
       <p className='text-6xl font-bold'>Code Constellation</p>
       <p className='text-3xl font-semibold'>By Fahad Hussain</p>
     </main>
