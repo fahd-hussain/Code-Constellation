588fc324015b5470cf74fe9a2937cf1c6ecfde18
Fahad Hussain <fahd__hussain@hotmail.com>
2024-07-18 16:58:42 +0500

    Integrated CLSX for multiple and conditional className

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import clsx from 'clsx'
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-4 bg-cyan-100'>
-      <p className='text-6xl font-bold'>Code Constellation</p>
-      <p className='text-3xl font-semibold'>By Fahad Hussain</p>
+    <main className={clsx('flex min-h-screen flex-col items-center justify-center gap-4')}>
+      <p className={clsx('text-6xl font-bold', 'text-gray900')}>Code Constellation</p>
+      <p className={clsx('text-3xl font-semibold', 'text-gray600')}>By Fahad Hussain</p>
+      <button
+        className={clsx(
+          'bg-cyan500 text-white200',
+          'py-2 px-4 rounded-lg',
+          'text-lg font-semibold'
+        )}>
+        Get Started
+      </button>
     </main>
   )
 }
