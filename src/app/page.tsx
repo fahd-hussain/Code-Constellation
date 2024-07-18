cc88ec57a5da67c3fbc763ccdac48be1641f4e89
Fahad Hussain <fahd__hussain@hotmail.com>
2024-07-18 22:06:04 +0500

    Added Navigation bar and hero background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
 
-import clsx from 'clsx'
+import VideoBackground from 'ui/video-background/video-background'
+
+// import clsx from 'clsx'
 
 export default function Home() {
-  return (
-    <main className={clsx('flex min-h-screen flex-col items-center justify-center gap-4')}>
-      <p className={clsx('text-6xl font-bold', 'text-gray900')}>Code Constellation</p>
-      <p className={clsx('text-3xl font-semibold', 'text-gray600')}>By Fahad Hussain</p>
-      <button
-        className={clsx(
-          'bg-cyan500 text-white200',
-          'py-2 px-4 rounded-lg',
-          'text-lg font-semibold'
-        )}>
-        Get Started
-      </button>
-    </main>
-  )
+  return <VideoBackground />
 }
---
+++ src/ui/index.ts
@@ -0,0 +1,1 @@
+export { default } from './video-background/video-background'
